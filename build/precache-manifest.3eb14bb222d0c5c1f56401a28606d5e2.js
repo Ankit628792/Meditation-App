a3bcd407cd1fb6e00c9862fabc832f5d0b5c5798
Ankit628792 <ankit628792@gmail.com>
2021-02-21 15:14:59 +0530

    Meditation App

---
+++ build/precache-manifest.3eb14bb222d0c5c1f56401a28606d5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735bd928c2508c909c64",
+    "url": "/static/css/main.dcf34051.chunk.css"
+  },
+  {
+    "revision": "735bd928c2508c909c64",
+    "url": "/static/js/main.aaa6f7de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "834e62c114ea96692c39",
+    "url": "/static/css/2.6b084025.chunk.css"
+  },
+  {
+    "revision": "834e62c114ea96692c39",
+    "url": "/static/js/2.ae2384ed.chunk.js"
+  },
+  {
+    "revision": "818b99ac34bba51ef25cb574d3d6da93",
+    "url": "/index.html"
+  }
+];
